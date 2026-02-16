db3de10719674b99a20945d70c8a714e11ef45eb
Ignas C <15948796+ignasc@users.noreply.github.com>
2026-02-16 19:20:53 +0000

    Add image to be shown when no image is found.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import cards from "./DB_content";
 import "./resetcss.css";
 import "./styles.css";
+import img_none from "./img/no-image-icon-23494.png";
 
 // Generate a list of cards
 const mainContent = document.getElementsByTagName("main")[0];
 mainContent.innerHTML = "";
-console.log(cards);
 const cardContainer = document.createElement("div");
 cardContainer.classList.add("card");
 
@@ -16,6 +16,7 @@ for (let i = 0; i < cards.length; i++) {
     const cardHeader = document.createElement("h1");
     const cardDescription = document.createElement("p");
 
+    cardImage.src = img_none;
     cardHeader.innerHTML = cardData.title;
     cardDescription.innerHTML = cardData.desc;
 
